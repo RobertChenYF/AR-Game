65ed5220bfa6665bce3e3cffca3d7ec9da6c9275
Robert Chen <chen1fan@icloud.com>
2021-10-08 02:51:03 -0400

    add spawn manager

---
+++ Assets/ObjectSpawnManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ObjectSpawnManager : MonoBehaviour
+{
+    [SerializeField] private ARRaycastManager raycastManager;
+    private List<ARRaycastHit> raycastHit = new List<ARRaycastHit>();
+    public List<GameObject> levels;
+   
+    [SerializeField]private GameObject spawnedObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (raycastManager.Raycast(touch.position, raycastHit))
+            {
+                if (touch.phase == TouchPhase.Began)
+                {
+                    
+                    spawnedObject.transform.position = raycastHit[0].pose.position;
+                }
+                else if (touch.phase == TouchPhase.Moved)
+                {
+                    if (spawnedObject != null)
+                    {
+                        spawnedObject.transform.position = raycastHit[0].pose.position;
+                    }
+                }
+                else if (touch.phase == TouchPhase.Ended)
+                {
+
+                }
+            }
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+
+    }
+}
